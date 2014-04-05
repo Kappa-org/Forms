6a4ef42b902ed5ce27a6363565f8ad1b13ebf557
Budry <zarubaondra@gmail.com>
2014-04-05 20:12:50 +0200

    Separated from kappa/framework

---
+++ src/Kappa/Forms/Controls/AntiSpam.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the Kappa package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Forms\Controls;
+
+use Nette\Forms\Controls\TextBase;
+
+/**
+ * Class AntiSpam
+ * @package Kappa\Forms\Controls
+ */
+class AntiSpam extends TextBase
+{
+	/**
+	 * @param string|null $label
+	 */
+	public function __construct($label = null)
+	{
+		parent::__construct($label);
+		$this->control->type = "text";
+	}
+
+	/**
+	 * @return \Nette\Utils\Html
+	 */
+	public function getControl()
+	{
+		$control = parent::getControl();
+		$control->addAttributes(array('data-kappa' => 'Kappa-checkControl'));
+		return $control;
+	}
+}
