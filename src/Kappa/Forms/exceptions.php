8ddbbe70c15189848e2f24692fc14752b8cede2d
Budry <zarubaondra@gmail.com>
2014-12-30 17:13:14 +0100

    Added ItemNotFound exception

---
+++ src/Kappa/Forms/exceptions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the Kappa\Forms package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Forms;
+
+/**
+ * Class ItemNotFoundException
+ *
+ * @package Kappa\Forms
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class ItemNotFoundException extends \LogicException
+{
+
+}
