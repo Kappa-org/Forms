18a9d43a9db7fb0a3a611f7a3e45c12413a91171
Budry <zarubaondra@gmail.com>
2014-12-30 17:14:37 +0100

    Added SelectBox

---
+++ src/Kappa/Forms/Controls/SelectBox.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of the Kappa\Forms package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\Forms\Controls;
+
+use Kappa\Forms\ItemNotFoundException;
+
+/**
+ * Class SelectBox
+ *
+ * @package Kappa\Forms\Controls
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class SelectBox extends \Nette\Forms\Controls\SelectBox
+{
+	/**
+	 * @param mixed $identifier
+	 */
+	public function removeItem($identifier)
+	{
+		if (!array_key_exists($identifier, $this->items)) {
+			throw new ItemNotFoundException("Item with '{$identifier}' index has not been found");
+		}
+		unset($this->items[$identifier]);
+	}
+}
